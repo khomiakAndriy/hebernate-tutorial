0cb477d128d660654cd6e4c5ee0b52ae3ce6dbc5
khomiakAndriy <khomiak.andriy@gmail.com>
2017-07-15 14:39:20 +0300

    Initial commit

---
+++ src/com/luv2code/hibernate/demo/CreateStudentDemo.java
@@ -0,0 +1,36 @@
+package com.luv2code.hibernate.demo;
+
+import com.luv2code.hibernate.demo.entity.Student;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class CreateStudentDemo {
+    public static void main(String[] args) {
+
+        // create session factory
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Student.class)
+                .buildSessionFactory();
+
+        // create session
+        Session session = factory.getCurrentSession();
+        try {
+            System.out.println("Creating new student object...");
+            Student student = new Student("Andriy", "Khomiak", "k@gmail.com");
+
+            session.beginTransaction();
+
+            System.out.println("Saving the student");
+            session.save(student);
+
+            session.getTransaction().commit();
+            System.out.println("Done!");
+
+        } finally {
+            factory.close();
+        }
+
+    }
+}
